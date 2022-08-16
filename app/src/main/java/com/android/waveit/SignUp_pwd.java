5a514ebb7a929868d4992c70a065038596f3c3d5
joohyun2 <ysm1459@naver.com>
2022-08-16 17:00:05 +0900

    joohyun

--- app/src/main/java/com/android/waveit/SignUp_pwd.java
+++ app/src/main/java/com/android/waveit/SignUp_pwd.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -39,4 +41,21 @@ public class SignUp_pwd extends AppCompatActivity {
             }
         });
     }
+
+    private TextWatcher sEditPWDTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
 }
