7c8f8f91d634d0bba8c6d2ea1d041eebd3feb37c
wlsskdbwls <63196091+wlsskdbwls@users.noreply.github.com>
2022-08-16 22:03:00 +0900

    StoreMain -> StoreLarge 전환

--- app/src/main/java/com/android/waveit/StoreMain.java
+++ app/src/main/java/com/android/waveit/StoreMain.java
@@ -1,4 +1,29 @@
 package com.android.waveit;
 
-public class StoreMain {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class StoreMain extends AppCompatActivity {
+
+    TextView btn_large;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store_main);
+
+        btn_large = findViewById(R.id.btn_large);
+        btn_large.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),StoreLarge.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
