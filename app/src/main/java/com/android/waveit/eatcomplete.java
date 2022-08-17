d2fe04e7b7fa0e06565f281f3efb0b929a157838
joohyun2 <ysm1459@naver.com>
2022-08-17 20:37:50 +0900

    joohyun

--- app/src/main/java/com/android/waveit/eatcomplete.java
+++ app/src/main/java/com/android/waveit/eatcomplete.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class eatcomplete extends AppCompatActivity {
 
     private Button btncomplete;
+    private ImageButton x;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +19,20 @@ public class eatcomplete extends AppCompatActivity {
         setContentView(R.layout.activity_eatcomplete);
 
         btncomplete = findViewById(R.id.btncomplete);
+        x = findViewById(R.id.x);
 
         btncomplete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Notification2.class);
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        x.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
             }
         });
