a869df192f8b10b2535e532013a1ce75c880e9d2
sohee7528 <sohee7528@hanmail.net>
2022-08-17 13:19:20 +0900

    add Information_product

--- app/src/main/java/com/android/waveit/Information_product.java
+++ app/src/main/java/com/android/waveit/Information_product.java
@@ -2,18 +2,13 @@ package com.android.waveit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageButton;
 
-public class ProductInformation extends AppCompatActivity {
-
+public class Information_product extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_product_information);
-
+        setContentView(R.layout.information_product);
     }
 }
