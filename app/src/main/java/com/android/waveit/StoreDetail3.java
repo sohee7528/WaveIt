606c0f37a3460e498635aec223df52bd95a0d7d8
wlsskdbwls <63196091+wlsskdbwls@users.noreply.github.com>
2022-08-17 23:13:16 +0900

    뷰페이저

--- app/src/main/java/com/android/waveit/StoreDetail3.java
+++ app/src/main/java/com/android/waveit/StoreDetail3.java
@@ -8,13 +8,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class StoreDetail1 extends Fragment {
+public class StoreDetail3 extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(
-                R.layout.activity_store_detail1, container, false);
+                R.layout.activity_store_detail3, container, false);
 
         return rootView;
     }
