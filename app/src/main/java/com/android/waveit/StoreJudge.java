5e349ab3324646fc871bd234e5dc72de6ec4e142
wlsskdbwls <63196091+wlsskdbwls@users.noreply.github.com>
2022-08-18 03:04:16 +0900

    추천. 비추천

---
+++ app/src/main/java/com/android/waveit/StoreJudge.java
@@ -0,0 +1,14 @@
+package com.android.waveit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StoreJudge extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store_judge);
+    }
+}
